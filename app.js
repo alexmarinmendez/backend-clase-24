eaa96e599625d993b4f7a031f1ab46a007464338
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-11-08 19:56:43 -0500

    feat: add session in file

--- app.js
+++ app.js
@@ -16,3 +16,16 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }))
+
+
+app.get('/', (req, res) => {
+    req.session.user = {
+        username: 'alex',
+        role: 'admin'
+    }
+    res.send({message: 'ok'})
+})
+
+app.get('/currentSession', (req, res) => {
+    res.send(req.session.user)
+})
